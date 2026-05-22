98a1c11b4cb92e552030fd67987ead01c7c2078d
Myroshnychenko Maxym <myroshnychenko@Myroshnychenkos-MacBook-Pro.local>
2026-05-22 15:17:52 +0300

    feat: initial project setup with SwiftUI + SwiftData container

---
+++ Application-ios/Application-ios/Application_iosApp.swift
@@ -0,0 +1,32 @@
+//
+//  Application_iosApp.swift
+//  Application-ios
+//
+//  Created by Myroshnychenko Maxym on 22.05.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Application_iosApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
